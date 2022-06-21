9e0a00eea147dc9e70fb9bac87b52ddc49af4fc3
Eunseo Kwak <xxxdmstj@gmail.com>
2022-06-21 11:31:44 +0900

    입력한 데이터 배열에 추가하고, 화면에 띄우는 것 까지. (이전상태업데이트 setFunction( ()=>{return [ ]})랑 map을 이용해서 컴포넌트 띄우는거 단순화 배열.map (배열요소 => {< 컴포넌트 속성={}>})

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ShoppingList from "./components/ShoppingList";
 import NewShoppingList from "./components/NewShoppingList";
 
-const items = [
+const example = [
   {
     id: Math.random().toString(),
     product: "Ground beef",
@@ -22,17 +22,28 @@ const items = [
   },
 ];
 
-const addItemHandler = (newItem) => { /*여기서 그 이전 상태 기반으로 하는 state */
-  /* 위에 배열에 추가!*/
-  console.log("App.js");
-  console.log({newItem});
-}
-
 function App() {
+
+  const [item, setItem] = useState(example); //여기서 상태 관리, 배열에 값 추가하고...
+
+  const addItemHandler = (newItem) => {
+    /*여기서 그 이전 상태 기반으로 하는 state */
+    /* 위에 배열에 추가!*/
+    console.log("App.js");
+    console.log({ newItem });
+
+    /* 이전 상태를 기반으로 하는 업데이트 */ 
+    setItem( (example) => {
+      return [newItem, ...example]
+    }
+    );
+    console.log({ item });
+  };
+
   return (
     <div className="shoppinglist">
-      <ShoppingList _items={items} />
-      <NewShoppingList onAddItem={addItemHandler}/>
+      <ShoppingList _items={item} />
+      <NewShoppingList onAddItem={addItemHandler} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ShoppingList.css";
 import AddShoppingList from "./AddShoppingList";
 import ShoppingListItem from "./ShoppingListItem";
 
 const ShoppingList = (props) => {
+  const [items, setItems] = useState(props._items);
+  console.log("ShoppingList.js App에서 props로 받은 데이터들 확인")
+  console.log(props._items);
+
   return (
     <div className="shoppinglist">
       <div className="shoppinglist__title">
       <span className="shoppinglist__title name">
         Shopping List
       </span>
-        </div>
-      
-      <ShoppingListItem
-        product={props._items[0].product}
-        quantity={props._items[0].quantity}
-      />
-      <ShoppingListItem
-        product={props._items[1].product}
-        quantity={props._items[1].quantity}
-      />
-      <ShoppingListItem
-        product={props._items[2].product}
-        quantity={props._items[2].quantity}
-      />
+      </div>
+      {
+        props._items.map( (item) => 
+          <ShoppingListItem 
+            product={item.product} quantity={item.quantity}
+            key={item.id}
+          />
+        )
+      }
+
     </div>
   );
 };
